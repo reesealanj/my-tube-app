f2dd6c756a889dc6173a32a5a61675c5f9638184
Reese Jones <reesealanj@gmail.com>
2022-06-26 11:33:40 -0500

    Implemented basic video grid

--- src/components/VideoGrid/VideoGrid.jsx
+++ src/components/VideoGrid/VideoGrid.jsx
@@ -3,14 +3,32 @@ import VideoGridItem from './VideoGridItem'
 
 const VideoGrid = ({ videos, classes }) => {
 
+  const videoThumbnails = [
+    '/img/thumbnail-1.jpg',
+    '/img/thumbnail-2.jpg',
+    '/img/thumbnail-3.jpg',
+    '/img/thumbnail-4.jpg',
+    '/img/thumbnail-5.jpg',
+  ]
+
+  const getRandomVideoThumb = () => {
+    return videoThumbnails[Math.floor((Math.random() * videoThumbnails.length))]
+  }
+
   const containerClasses = classNames(classes, 
-    'grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2'
+    'grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-4 cursor-pointer'
   )
 
   return (
     <div className={containerClasses}>
       { videos && videos.map((video, idx) => {
-        return (<VideoGridItem video={video} classes='' key={`video-${idx}`} />)
+        return (
+          <VideoGridItem 
+            video={video}
+            videoThumb={getRandomVideoThumb()}
+            classes='' 
+            key={`video-${idx}`} 
+          />)
       })}
     </div>
   )
--- src/components/VideoGrid/VideoGridItem.jsx
+++ src/components/VideoGrid/VideoGridItem.jsx
@@ -1,7 +1,65 @@
-const VideoGridItem = ({ video, classes }) => {
-  return (<div className={classes}>
-    {video.title}
-  </div>)
+import { useState } from 'react'
+import Image from "next/image"
+import classNames from 'classnames'
+import { useRouter } from 'next/router'
+
+const VideoGridItem = ({ video, videoThumb, classes }) => {
+
+  const THUMBNAIL_WIDTH = 180
+  const THUMBNAIL_HEIGHT = 1620/16
+
+  const [hovered, setHovered] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+  const lengthPillClasses = classNames('rounded-md absolute top-[55%] right-1 text-white font-normal', {
+    'bg-stone-500': hovered,
+    'bg-stone-700': !hovered
+  })
+
+  const playButtonClasses = classNames('absolute top-[30%] left-[45%] text-2xl', {
+    'invisible': !hovered
+  })
+
+  const containerClasses = classNames(classes, 
+    'flex flex-col relative rounded-md', {
+      'bg-stone-200': !hovered,
+      'bg-stone-100 font-semibold': hovered
+    })
+
+  const imageClasses = classNames({
+    '': hovered
+  })
+
+  const router = useRouter()
+
+  return (
+    <div 
+      className={containerClasses} 
+      onMouseEnter={() => {setHovered(true)}} 
+      onMouseLeave={() => {setHovered(false)}}
+      onClick={() => {router.push(`/video/${video.id}`)}}
+    >
+      <Image 
+          src={videoThumb}
+          width={THUMBNAIL_WIDTH * 1.25}
+          height={THUMBNAIL_HEIGHT * 1.25}
+          alt={video.description}
+          className={imageClasses}
+      />
+      <div className={playButtonClasses}>
+        ⏯
+      </div>
+      <div className={lengthPillClasses}>
+        {video.videoLength}
+      </div>
+      <div className='inline-flex flex-row items-baseline justify-between px-4'>
+        <span className='text-base'>{video.title}</span>
+        <span className='text-xs'>{video.views} views</span>
+      </div>
+      <div className='inline-flex flex-row items-baseline px-4 pb-2'>
+        <span className='text-xs text-muted'>@{video.author}</span>
+      </div>
+    </div>)
 }
 
 export default VideoGridItem
